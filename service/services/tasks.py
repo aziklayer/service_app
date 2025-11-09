0e6086ed1e0aa46cb8c780e5ea541bdc8da9e598
azamat <azik8423@gmail.com>
2025-11-09 13:05:40 +0500

    day 7

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,7 +1,9 @@
+import datetime
 import time
 
 from celery import shared_task
 from celery_singleton import Singleton
+from django.db import transaction
 from django.db.models import F
 
 
@@ -10,14 +12,25 @@ from django.db.models import F
 @shared_task(base=Singleton)
 def set_price(subscription_id):
     from services.models import Subscription
+    with transaction.atomic():
 
-    time.sleep(5)
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
+            annotated_price=F('service__full_price') -
+                              F('service__full_price') * F('plan__discount_percent') / 100.00).first()
 
-    subscription = Subscription.objects.get(id=subscription_id).annotate(
-        annotated_price=F('service__full_price') -
-                          F('service__full_price') * F('plan__discount_percent') / 100.00).first()
+        subscription.price = subscription.annotated_price
+        subscription.save()
 
 
-    subscription.price = subscription.annotated_price
-    subscription.save()
 
+
+@shared_task(base=Singleton)
+def set_comment(subscription_id):
+    from services.models import Subscription
+
+    with transaction.atomic():
+
+        subscription = Subscription.objects.select_for_update().get(id=subscription_id)
+
+        subscription.comment = str(datetime.datetime.now())
+        subscription.save()
